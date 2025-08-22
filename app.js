0a4b30a16532af5e3fdc3746c6f1e0ef7b0e3b2e
unknown <juantdtd@gmail.com>
2025-08-22 18:03:01 -0500

    Implementacion del js logica Amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+function agregarAmigo(){
+
+    let nombre = document.getElementById("amigo").value //recibiendo el nombre del usuario 
+    
+    if (nombre === ""){  //Esto es para verificar ingresen un nombre y no este vacio.
+        alert("Por favor, escribe un nombre")
+    }else {
+        listaNombres.push(nombre);
+        limpiarCampo();
+        mostrarNombres();
+    }
+}
+//Aca se sortea al amigo usando el lenght para recorrer 
+function sortearAmigo(){
+    if (listaNombres.length == 0 ){
+        alert("No hay nombres para sortear");
+    }else {
+        let indiceSorteado = Math.floor((Math.random()*listaNombres.length));
+        let nombreSorteado = listaNombres[indiceSorteado];
+        document.getElementById("resultado").innerHTML = `El amigo secreto es: <strong style="color:green">${nombreSorteado}</strong>`;
+    }
+}
+
+function mostrarNombres (){
+   document.getElementById("listaAmigos").innerHTML = listaNombres.join("<br>");
+
+}
+
+
+function limpiarCampo() { //LIMPIA EL CAMPO DE ENTRA CON ID AMIGO
+    document.getElementById('amigo').value = "";
+}
+
+
+
